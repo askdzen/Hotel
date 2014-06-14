dc2eae1e31e92850f511cbca083968a32f37ae58
askdzen <ask.dzen@mail.ru>
2014-06-14 22:03:15 +0600

    refactoring, add enum

--- src/main/java/com/epam/ad/hotel/entity/ResidentRoom.java
+++ src/main/java/com/epam/ad/hotel/entity/ResidentRoom.java
@@ -2,74 +2,87 @@ package com.epam.ad.hotel.entity;
 
 import java.math.BigDecimal;
 
+enum roomType {SINGLEROOM, DOOBLEROOM, TRIPLEROOM};
+
 public class ResidentRoom extends Room {
-    private String type; //тип номера (одноместный, двух-трех:местный)
+
     private Boolean breakfast;
-    private  Boolean wiFi;
+    private Boolean wiFi;
+    private roomType roomType;
 
-    public static class ResidentRoomBuilder{
-        private BigDecimal price =BigDecimal.ZERO;
-        private String type="";
-        private Boolean breakfast=false;
-        private  Boolean wiFi=false;
-
-    public ResidentRoomBuilder type(String val){
-        type=val;return this;
+    public ResidentRoom(ResidentRoomBuilder builder) {
+        price = builder.price;
+        roomType = builder.roomType;
+        breakfast = builder.breakfast;
+        wiFi = builder.wiFi;
     }
-        public ResidentRoomBuilder initialCost(BigDecimal val){
-            price =val;return this;
-        }
-        public ResidentRoomBuilder breakfast(Boolean val){
-            breakfast=val;return this;
-        }
-        public ResidentRoomBuilder wiFi(Boolean val){
-            wiFi=val;return this;
-        }
-        public ResidentRoom build(){return new ResidentRoom(this);}
-    }
-
-
-        public ResidentRoom(ResidentRoomBuilder builder){
-            price =builder.price;
-            type=builder.type;
-            breakfast=builder.breakfast;
-            wiFi=builder.wiFi;
-        }
 
     @Override
     public String toString() {
         return "ResidentRoom{" +
-                "type='" + type + '\'' +
+                "roomType='" + roomType + '\'' +
                 ", breakfast=" + breakfast +
-                ", wiFi=" + wiFi +"}"+
+                ", wiFi=" + wiFi + "}" +
                 "\n";
     }
 
     @Override
-    public BigDecimal GetCurrentCost() {
+    public BigDecimal getCurrentCost() {
         return price;
     }
-    public String getType() {
-        return type;
+
+    public roomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(roomType roomType) {
+        this.roomType = roomType;
     }
 
     public Boolean getBreakfast() {
         return breakfast;
     }
 
-    public Boolean getWiFi() {
-        return wiFi;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
     public void setBreakfast(Boolean breakfast) {
         this.breakfast = breakfast;
     }
 
+    public Boolean getWiFi() {
+        return wiFi;
+    }
+
     public void setWiFi(Boolean wiFi) {
         this.wiFi = wiFi;
+    }
+
+    public static class ResidentRoomBuilder {
+        private BigDecimal price = BigDecimal.ZERO;
+        private Boolean breakfast = false;
+        private Boolean wiFi = false;
+        private roomType roomType;
+
+        public ResidentRoomBuilder roomType(roomType val) {
+            roomType = val;
+            return this;
+        }
+
+        public ResidentRoomBuilder initialCost(BigDecimal val) {
+            price = val;
+            return this;
+        }
+
+        public ResidentRoomBuilder breakfast(Boolean val) {
+            breakfast = val;
+            return this;
+        }
+
+        public ResidentRoomBuilder wiFi(Boolean val) {
+            wiFi = val;
+            return this;
+        }
+
+        public ResidentRoom build() {
+            return new ResidentRoom(this);
+        }
     }
 }
